0ba91ea1ebcc40f1bb9da252f7a8585abedd78b8
jhiney <hiney.jake@outlook.com>
2022-03-24 13:55:35 -0500

    rough outline of portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,9 +1,21 @@
 export default function Portfolio() {
 	return (
-		<div>
-			<div className="p-3 bg-white shadow rounded-lg">
-					<h3 className="text-xl border-b">font-serif</h3>
-					<p className="font-serif">The quick brown fox jumps over the lazy dog.</p>
+		<div className="bg-black min-h-screen font-mono">
+			<div className="container text-center pb-20">
+				<p className=" font-semibold text-6xl text-white">
+					Portfolio
+					<br></br>
+					$50,000
+				</p>
+			</div>
+			<div className="grid grid-cols-3 gap-4 text-center text-3xl text-white">
+				<div>Company</div>
+				<div>Shares Owned</div>
+				<div>Price of investment</div>
+				<div>Apple, Inc.</div>
+				<div>112</div>
+				<div>$39,000
+				</div>
 			</div>
 		</div>
 	);
