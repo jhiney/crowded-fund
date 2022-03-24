d79f14078a600356a1cca85f07baffb02174252d
jhiney <hiney.jake@outlook.com>
2022-03-24 10:56:18 -0500

    boilerplate portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+	return (
+		<div>
+			<div className="p-3 bg-white shadow rounded-lg">
+					<h3 className="text-xl border-b">font-serif</h3>
+					<p className="font-serif">The quick brown fox jumps over the lazy dog.</p>
+			</div>
+		</div>
+	);
+}
