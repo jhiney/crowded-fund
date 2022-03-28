8efee1ec90866c5b6dd6f3f52f682e5baaa39e38
jhiney <hiney.jake@outlook.com>
2022-03-28 12:38:32 -0500

    extract header to component

---
+++ components/positionHeader.js
@@ -0,0 +1,9 @@
+export default function PositionHeader() {
+	return (
+		<div className="grid grid-cols-3 gap-4 text-center text-3xl text-white">
+			<div>Company</div>
+			<div>Shares Owned</div>
+			<div>Price of investment</div>
+		</div>
+	);
+}
