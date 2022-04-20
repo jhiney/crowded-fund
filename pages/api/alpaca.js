1b0550a956f09f80e175b434325f3465a767f2bc
jhiney <hiney.jake@outlook.com>
2022-04-20 14:56:21 -0500

    resolve request

--- pages/api/alpaca.js
+++ pages/api/alpaca.js
@@ -16,26 +16,22 @@ export async function getPositionData() {
 	return positions;
 }
 
-
-
 export default async function handler(req, res) {
 	if (req.method !== "POST") {
 		res.status(405).send({ message: "Only POST requests allowed" });
 		return;
 	}
-    console.log(req.body.symbol);
-    console.log(req.body.qty);
-    console.log(req.body.side);
 
-
-	const order = await alpaca.createOrder({
-        symbol: req.body.symbol, // any valid ticker symbol
-        qty: req.body.qty, // qty or notional required, not both
-        side: req.body.side, // 'buy' or 'sell'
-        type: 'market',
-        time_in_force: 'day' 
-      })
-	return order;
-
-
+	try {
+		await alpaca.createOrder({
+			symbol: req.body.symbol, // any valid ticker symbol
+			qty: req.body.qty, // qty or notional required, not both
+			side: req.body.side, // 'buy' or 'sell'
+			type: "market",
+			time_in_force: "day"
+		});
+		res.status(200).end();
+	} catch (error) {
+		res.status(error).json({});
+	}
 }
