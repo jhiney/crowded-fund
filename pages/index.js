c96c324da5261c6784d1c101e896e63d1c048796
jhiney <hiney.jake@outlook.com>
2022-04-22 08:55:21 -0500

    changes to default home page

--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,8 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
+					This is a terrible idea.
 				</h1>
-
-				<p className={styles.description}>
-					Get started by editing <code className={styles.code}>pages/index.js</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}>
-						<h2>Deploy &rarr;</h2>
-						<p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-					</a>
-				</div>
-				<div className="p-3 bg-white shadow rounded-lg">
-					<h3 className="text-xl border-b">font-serif</h3>
-					<p className="font-serif">The quick brown fox jumps over the lazy dog.</p>
-				</div>
 			</main>
 
 			<footer className={styles.footer}>
