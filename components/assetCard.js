c7547bfce0eeb1171a22a630219e05960fe8e8e2
jhiney <hiney.jake@outlook.com>
2022-04-26 07:22:48 -0500

    notes

--- components/assetCard.js
+++ components/assetCard.js
@@ -3,7 +3,7 @@ export default function AssetCard() {
 		<div className="max-w-sm rounded overflow-hidden border-2 border-zinc-200 m-auto text-center">
 			<div className="px-6 py-4">
 				<div className="font-bold text-xl mb-2 text-white">$AAPL</div>
-				<p className=" text-base text-zinc-200">Apple, Inc.</p>
+				<p className=" text-base text-zinc-200">Apple, Inc.</p> {/* opened at {price} (green if greater than last close, red if not) */}
 			</div>
 			<div className="grid grid-cols-2 gap-2 px-6 pt-4 pb-8">
 				<button className="bg-transparent hover:bg-green-600 text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-transparent rounded text-center">
