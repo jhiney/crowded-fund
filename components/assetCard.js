b0f0380a12ca0c95630c0f2e02b4ecd7388c84e8
jhiney <hiney.jake@outlook.com>
2022-04-26 15:21:48 -0500

    passing state to testing

--- components/assetCard.js
+++ components/assetCard.js
@@ -1,15 +1,26 @@
-export default function AssetCard() {
+export default function AssetCard({ setSideValue }) {
+	
+	const SideButtonClicked = (e) => {
+		setSideValue(e.target.value);
+	  };
+		
 	return (
 		<div className="max-w-sm rounded overflow-hidden border-2 border-zinc-200 m-auto text-center">
 			<div className="px-6 py-4">
 				<div className="font-bold text-xl mb-2 text-white">$AAPL</div>
-				<p className=" text-base text-zinc-200">Apple, Inc.</p> {/* opened at {price} (green if greater than last close, red if not) */}
+				<p className=" text-base text-zinc-200">Apple, Inc</p> {/* opened at {price} (green if greater than last close, red if not) */}
 			</div>
 			<div className="grid grid-cols-2 gap-2 px-6 pt-4 pb-8">
-				<button className="bg-transparent hover:bg-green-600 text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-transparent rounded text-center">
+				<button 
+				value="buy"
+				onClick={SideButtonClicked}
+				className="bg-transparent hover:bg-green-600 text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-transparent rounded text-center">
 					Buy
 				</button>
-				<button className="bg-transparent hover:bg-red-600 text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-transparent rounded text-center">
+				<button 
+				value="sell"
+				onClick={SideButtonClicked}
+				className="bg-transparent hover:bg-red-600 text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-transparent rounded text-center">
 					Sell
 				</button>
 			</div>
--- pages/testing.js
+++ pages/testing.js
@@ -1,9 +1,14 @@
 import AssetCard from "../components/assetCard";
+import { useState } from "react";
+
 
 export default function Testing(props) {
+	const [sideValue, setSideValue] = useState('');
+	
 	return (
 		<div className="bg-zinc-900 min-h-screen font-mono flex h-screen">
-			<AssetCard />
+			<h1 className="text-white">{sideValue}</h1>
+			<AssetCard setSideValue = {setSideValue} />
 		</div>
 	);
 }
