9ef7c10010013d50eecda1b8cd323d33f5434d2f
jhiney <hiney.jake@outlook.com>
2022-04-28 08:47:27 -0500

    lift state from card through grid

--- components/assetCard.js
+++ components/assetCard.js
@@ -1,4 +1,4 @@
-export default function AssetCard({ sideValue, setSideValue, closePrice }) {
+export default function AssetCard({ sideValue, setSideValue, close, open, quote }) {
 	const SideButtonClicked = (e) => {
 		setSideValue(e.target.value);
 	};
@@ -6,8 +6,10 @@ export default function AssetCard({ sideValue, setSideValue, closePrice }) {
 	return (
 		<div className="max-w-sm rounded overflow-hidden border-2 border-zinc-200 m-auto text-center">
 			<div className="px-6 py-4">
-				<div className="font-bold text-xl mb-2 text-white">$AAPL - $162.43</div>
-				<p className=" text-base text-zinc-200">Apple, Inc closed yesterday at ${closePrice} and opened today at $160.13</p>
+				<div className="font-bold text-xl mb-2 text-white">$AAPL - ${quote}</div>
+				<p className=" text-base text-zinc-200">
+					Apple, Inc closed yesterday at ${close} and opened today at ${open}
+				</p>
 				{/* opened at {price} (green if greater than last close, red if not) */}
 			</div>
 			<div className="grid grid-cols-2 gap-2 px-6 pt-4 pb-8">
--- pages/testing.js
+++ pages/testing.js
@@ -1,29 +1,29 @@
-import AssetCard from "../components/assetCard";
+import AssetGrid from "../components/assetGrid";
 import { useState } from "react";
 
-import { getStockSnapshot  } from "../pages/api/alpaca";
+import { getStockSnapshot } from "../pages/api/alpaca";
 
 export async function getServerSideProps() {
-	
-	const snapshot = await getStockSnapshot('AAPL');
-
+	const snapshot = await getStockSnapshot("AAPL");
 	const close = snapshot.DailyBar.ClosePrice;
-	
+	const open = snapshot.DailyBar.OpenPrice;
+	const quote = snapshot.LatestTrade.Price;
 
 	return {
 		props: {
-			close
+			close,
+			quote,
+			open
 		}
 	};
 }
 
-
 export default function Testing(props) {
-	const [sideValue, setSideValue] = useState('');
-	
+	const [sideValue, setSideValue] = useState("");
 	return (
 		<div className="bg-zinc-900 min-h-screen font-mono flex h-screen">
-			<AssetCard setSideValue = {setSideValue} sideValue = {sideValue} closePrice = {props.close} />
+			{sideValue}
+			<AssetGrid setSideValue={setSideValue} sideValue={sideValue} {...props} />
 		</div>
 	);
 }
