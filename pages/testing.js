91ebf336fe138f8b25e4b5864b446f1e2043749a
jhiney <hiney.jake@outlook.com>
2022-07-12 17:28:34 -0500

    add sort and documentation

--- pages/testing.js
+++ pages/testing.js
@@ -4,13 +4,18 @@ import { useState } from "react";
 import { getMultiStockSnapshot, getCompanyName } from "../pages/api/alpaca";
 
 export async function getServerSideProps() {
-	const symbols = ["AAPL", "GS", "JPM", "NET"];
-	const names = [];
+	const symbols = ["J", "GS", "AAPL", "NET"];
+	let names = [];
 	const multi = await getMultiStockSnapshot(symbols);
 
 	for (const symbol of symbols) {
 		names.push(await getCompanyName(symbol));
 	}
+
+	//Because multi stock snapshot runs in alphabetically order, we have to sort the names because those are pushed in the order of
+	//the symbols array above. If we didn't sort, the names would be mishmashed if they didn't happen to be in order already.
+	names.sort((a, b) => (a.symbol > b.symbol ? 1 : b.symbol > a.symbol ? -1 : 0));
+
 	return {
 		props: {
 			multi,
@@ -20,6 +25,8 @@ export async function getServerSideProps() {
 }
 
 export default function Testing(props) {
+	//This side value stores which side, Buy/Sell, is selected per CARD.
+	//It is initialized here as Testing is the most elevated component.
 	const [sideValue, setSideValue] = useState([]);
 	return (
 		<div className="bg-zinc-900 min-h-screen font-mono h-screen">
